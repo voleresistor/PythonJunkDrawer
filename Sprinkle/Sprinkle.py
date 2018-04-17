23a0f4a3926513d8b9d74a677637857519460fb2
Andrew Ogden <aogden@dxpe.corp>
2018-04-17 15:31:02 -0500

    Fleshing out some basic data activities

--- Sprinkle/Sprinkle.py
+++ Sprinkle/Sprinkle.py
@@ -1,28 +1,55 @@
 import json
+import datetime
 
+def get_json(file):
+    """
+    Load JSON data into memory
+    file: the path to the data file
+    """
+    with open(file) as json_file:
+        json_data = json.load(json_file)
+    json_file.close()
+    return json_data
+
+def save_json(file, data):
+    """
+    Save and close data file
+    file: path to the data file
+    data: data object to save
+    """
+    with open(file, 'w') as json_file:
+        json.dump(data, json_file)
+    json_file.close()
+
+# Gather current date and time
+#day_map = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3:'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday'}
+day = datetime.datetime.now().weekday()
+time = str(datetime.datetime.now().hour) + str(datetime.datetime.now().minute)
+
+# int(time) >= int(sched) - 1 & int(time) <= int(sched) + 1
+
+# Get source file from arguments
 sourcefile = 'x:\\Git\\Python\\Sprinkle\\test.conf'
 
-def get_json(f):
-    with open(f) as json_file:
-        d = json.load(json_file)
-    json_file.close()
-    return (d)
-
-def save_json(f, d):
-    with open(f, 'w') as json_file:
-        json.dump(d, json_file)
-    json_file.close()
-
+# Load the config file
 d = get_json(sourcefile)
-print (d["WaterDays"]["Thursday"]["Enabled"])
 
-if d["WaterDays"]["Thursday"]["Enabled"] == "0":
-    print("Enabling...")
-    d["WaterDays"]["Thursday"]["Enabled"] = "1"
-else:
-    print("Disabling...")
-    d["WaterDays"]["Thursday"]["Enabled"] = "0"
+# Determine schedule type
+if d["ScheduleType"]["Type"] == 'Static':
+    print("Running static schedule.")
+elif d["ScheduleType"]["Type"] == 'Interval':
+    print("Running interval schedule every %s day(s)." % d["ScheduleType"]["Interval"])
 
-print (d["WaterDays"]["Thursday"]["Enabled"])
+if d["WaterDays"][str(day)]["Enabled"] == '0':
+    water_schedule = d["WaterDays"][str(day)]["Schedule"]
+    for e in water_schedule:
+        if e["Time"] == time:
+            i = 1
+            for station in e["Duration"]:
+                print("Running Station %d for %s minutes." % (i, str(e["Duration"][i-1])))
+                i += 1
+        else:
+            #A timespan here to log time until water event?
+            print("Waiting.")
 
 save_json(sourcefile, d)
